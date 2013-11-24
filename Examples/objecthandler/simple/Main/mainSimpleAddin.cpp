d436a19cc16e39421eb3fbe231641289f499e82c
Eric Ehlers <eric.ehlers@nazcatech.be>
2013-11-24 01:23:05 -0800

    update VC

--- Examples/objecthandler/simple/Main/mainSimpleAddin.cpp
+++ Examples/objecthandler/simple/Main/mainSimpleAddin.cpp
@@ -4,7 +4,7 @@
 
 int main() {
 	std::cout << "hi" << std::endl;
-    std::cout << SimpleLibAddin::func() << std::endl;
+    std::cout << SimpleLibAddin::slFunc() << std::endl;
     SimpleLibAddin::slAdder("adder", 1);
 	std::cout << "1 + 2 = " << SimpleLibAddin::slAdderadd("adder", 2) << std::endl;
 	std::cout << "bye" << std::endl;
