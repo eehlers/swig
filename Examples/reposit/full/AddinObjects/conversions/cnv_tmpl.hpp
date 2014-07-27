41241c9541d96b6b44c74c1a41006fc64d52eeab
Eric Ehlers <eric.ehlers@nazcatech.be>
2014-07-27 18:21:24 +0200

    consolidate conversion code

---
+++ Examples/reposit/full/AddinObjects/conversions/cnv_tmpl.hpp
@@ -0,0 +1,38 @@
+
+#ifndef addinobjects_convert_cnv_tmpl_hpp
+#define addinobjects_convert_cnv_tmpl_hpp
+
+// This file contains some templates common to AddinObjects and AddinXl.
+// This file should be #included only by AddinXxx/conversions/xxx.cpp,
+
+#include <Library/adder2.hpp>
+#include <boost/lexical_cast.hpp>
+
+namespace ObjectHandler {
+
+    inline bool is_numeric(const std::string &s, long &l) {
+        try {
+            l = boost::lexical_cast<long>(s);
+            return true;
+        } catch(...) {
+            return false;
+        }
+    }
+
+    template<class container_t>
+    SimpleLib::Long convertLong(const container_t& c) {
+        if(c.type() == typeid(long))
+            return SimpleLib::Long(c.operator long());
+        else if(c.type() == typeid(std::string)) {
+            std::string s = c.operator std::string();
+            long l;
+            if (is_numeric(s, l))
+                return SimpleLib::Long(l);
+            else
+                OH_FAIL("unable to convert string '" << s << "' to type 'SimpleLib::Long'");
+        }
+        OH_FAIL("unable to convert type '" << c.type().name() << "' to type 'SimpleLib::Long'");
+    }
+}
+
+#endif
--- Examples/reposit/full/AddinXl/conversions/convert2.cpp
+++ Examples/reposit/full/AddinXl/conversions/convert2.cpp
@@ -1,33 +1,8 @@
 
 #include "convert2.hpp"
+#include "AddinObjects/conversions/cnv_tmpl.hpp"
 
 namespace ObjectHandler {
-
-    // FIXME consolidate with AddinObjects/conversions/convert2.cpp
-
-    inline bool is_numeric(const std::string &s, long &l) {
-        try {
-            l = boost::lexical_cast<long>(s);
-            return true;
-        } catch(...) {
-            return false;
-        }
-    }
-
-    template<class container_t>
-    SimpleLib::Long convertLong(const container_t& c) {
-        if(c.type() == typeid(long))
-            return SimpleLib::Long(c.operator long());
-        else if(c.type() == typeid(std::string)) {
-            std::string s = c.operator std::string();
-            long l;
-            if (is_numeric(s, l))
-                return SimpleLib::Long(l);
-            else
-                OH_FAIL("unable to convert string '" << s << "' to type 'SimpleLib::Long'");
-        }
-        OH_FAIL("unable to convert type '" << c.type().name() << "' to type 'SimpleLib::Long'");
-    }
 
     template<> 
     SimpleLib::Long convert2<SimpleLib::Long, ConvertOper>(const ConvertOper& c) {
