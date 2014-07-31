fa690a744c8dec488f6b6d1c63a3187c9fa5ab4b
Eric Ehlers <eric.ehlers@nazcatech.be>
2014-07-31 11:37:49 +0200

    refactor #includes

---
+++ Examples/reposit/full/AddinCpp/all.hpp
@@ -0,0 +1,14 @@
+
+#ifndef add_all_hpp
+#define add_all_hpp
+
+#include <AddinCpp/add_adder2.hpp>
+#include <AddinCpp/add_adder.hpp>
+#include <AddinCpp/add_color.hpp>
+#include <AddinCpp/add_foo.hpp>
+#include <AddinCpp/add_inheritance.hpp>
+#include <AddinCpp/add_noparm.hpp>
+#include <AddinCpp/init.hpp>
+
+#endif
+
--- Examples/reposit/full/Main/mainFullLibAddin.cpp
+++ Examples/reposit/full/Main/mainFullLibAddin.cpp
@@ -1,12 +1,6 @@
 
 #include <iostream>
-#include "AddinCpp/init.hpp"
-#include "AddinCpp/add_adder.hpp"
-#include "AddinCpp/add_adder2.hpp"
-#include "AddinCpp/add_color.hpp"
-#include "AddinCpp/add_foo.hpp"
-#include "AddinCpp/add_inheritance.hpp"
-#include "AddinCpp/add_noparm.hpp"
+#include "AddinCpp/all.hpp"
 #include "oh/addin.hpp"
 
 int main() {
