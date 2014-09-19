4f4d9082b035e1f9045e64c6c1d47ae140e74978
Eric Ehlers <eric.ehlers@nazcatech.be>
2014-09-19 22:59:05 +0200

    windows catching up

--- Examples/reposit/complex/ComplexLibXL/clxl/conversions/convert2.cpp
+++ Examples/reposit/complex/ComplexLibXL/clxl/conversions/convert2.cpp
@@ -1,12 +1,13 @@
-//
-//#include "conversions/convert2.hpp"
-//#include <clo/conversions/cnv_tmpl.hpp>
-//
-//namespace ObjectHandler {
-//
-//    template<> 
-//    ComplexLib::Long convert2<ComplexLib::Long, ConvertOper>(const ConvertOper& c) {
-//        return convertLong(c);
-//    }
-//}
-//
+
+//#include <clxl/conversions/convert2.hpp>
+#include "conversions/convert2.hpp"//FIXME
+#include <clo/conversions/cnv_tmpl.hpp>
+#include <ohxl/convert_oper.hpp>
+
+namespace ObjectHandler {
+
+    template<>
+    ComplexLib::Grade convert2<ComplexLib::Grade, ConvertOper>(const ConvertOper& p) {
+        return convertGrade(p);
+    }
+}
--- Examples/reposit/complex/ComplexLibXL/clxl/conversions/convert2.hpp
+++ Examples/reposit/complex/ComplexLibXL/clxl/conversions/convert2.hpp
@@ -1,17 +1,16 @@
-//
-//#ifndef addinxl_convert2_hpp
-//#define addinxl_convert2_hpp
-//
-//#include <boost/lexical_cast.hpp>
-//#include <ohxl/convert_oper.hpp>
-//#include <cl/adder2.hpp>
-//
-//namespace ObjectHandler {
-//
-//    template<> 
-//    ComplexLib::Long convert2<ComplexLib::Long, ConvertOper>(const ConvertOper& c);
-//}
-//
-//#endif
-//
-//
+
+#ifndef addinxll_convert2_hpp
+#define addinxll_convert2_hpp
+
+#include <oh/property.hpp>
+#include <cl/conversions.hpp>
+
+namespace ObjectHandler {
+
+    class ConvertOper;
+
+    template<> 
+    ComplexLib::Grade convert2<ComplexLib::Grade, ConvertOper>(const ConvertOper& p);
+}
+
+#endif
